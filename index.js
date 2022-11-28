a33f209cfe20b4f02cdcc3e37d7128c8455310f8
lassib <91030337+lassib@users.noreply.github.com>
2022-11-28 15:31:30 +0200

    3.9 done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -26,10 +27,13 @@ let persons = [
   },
 ];
 
-morgan.token('body', (req, res) => JSON.stringify(req.body))
+morgan.token("body", (req, res) => JSON.stringify(req.body));
 
+app.use(cors());
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
