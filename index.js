9f56019ab1d47518cc4387be316edc9d32009e64
lassib <91030337+lassib@users.noreply.github.com>
2022-12-06 16:43:21 +0200

    Updated folder structure part 3 done

--- index.js
+++ index.js
@@ -1,49 +1,33 @@
-require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const logger = require('./logger')
-
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const Person = require("./models/person");
+const morgan = require("morgan");
 
-const url = process.env.MONGO_URI;
-
-mongoose.connect(url);
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-  id: Number,
-});
-
-personSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Person = mongoose.model("Person", personSchema);
-
-let persons = [];
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
-
-app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
+app.use(cors());
+app.use(express.static("build"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-app.use(express.static("build"));
 
-app.get('/info', async (request, response) => {
-  const dbLength = await Person.countDocuments({})
+app.get("/info", async (request, response) => {
+  const dbLength = await Person.countDocuments({});
   response.send(`<h1>Info</h1>
   <p>Phonebook has info for ${dbLength} people</p>
-  <p>Received at ${new Date()}</p>`)
-})
+  <p>Received at ${new Date()}</p>`);
+});
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
@@ -69,36 +53,26 @@ app.delete("/api/persons/:id", (request, response, next) => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (
-    !body.name ||
-    !body.number ||
-    persons.find((person) => person.name == body.name)
-  ) {
-    if (!body.name) {
-      return response.status(400).json({
-        error: "name missing",
-      });
-    } else if (!body.number) {
-      return response.status(400).json({
-        error: "number missing",
-      });
-    } else {
-      return response.status(400).json({
-        error: "name must be unique",
-      });
-    }
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
     id: body.id,
-  };
+  });
 
-  new Person(person)
-    .save()
-    .then((savedPerson) => response.json(savedPerson))
-    .catch((error) => next(error));
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 app.put(`/api/persons:id`, (request, response, next) => {
@@ -107,6 +81,7 @@ app.put(`/api/persons:id`, (request, response, next) => {
   const person = {
     name: body.name,
     number: body.number,
+    id: body.id,
   };
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
@@ -115,16 +90,6 @@ app.put(`/api/persons:id`, (request, response, next) => {
     })
     .catch((error) => next(error));
 });
-
-const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
-  logger.info('---')
-  next()
-}
-
-app.use(requestLogger);
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
